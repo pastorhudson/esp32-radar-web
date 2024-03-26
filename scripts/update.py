5d13ead7a04ea2000613c014b5961bf516c38d4b
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2024-03-26 19:11:08 -0400

    Remove commented out lines in update.py
    The update function in update.py has been cleaned up to improve its readability and execution. Commented out lines that were previously halting the completion of the update process have been deleted.

--- scripts/update.py
+++ scripts/update.py
@@ -129,12 +129,10 @@ def download_latest_release_bin_assets(user, repo):
 
 
 def update():
-    # while True:
     print("Downloading Latest FormationFlight Firmware")
     download_latest_release_bin_assets("FormationFlight", "FormationFlight")
     print("Generating install Manifests")
     generate_manifests()
-    # time.sleep(60)
 
 
 if __name__ == "__main__":
