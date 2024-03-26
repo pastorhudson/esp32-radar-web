e2186e473d4a5b7f253804d715b137e2c3dce5a5
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2024-03-26 17:04:36 -0400

    Refactor update script and adjust Procfile
    This commit modifies the script update.py to continuously update every minute. Correspondingly, the path of the script called in the Procfile has been adjusted. Post deployment scripts in app.json have been commented out as part of these changes.

--- scripts/update.py
+++ scripts/update.py
@@ -1,12 +1,15 @@
+import time
 from download_assets import download_latest_release_bin_assets
 from generate_manifests import generate_manifests
 
 
 def update():
-    print("Downloading Latest FormationFlight Firmware")
-    download_latest_release_bin_assets("FormationFlight", "FormationFlight")
-    print("Generating install Manifests")
-    generate_manifests()
+    while True:
+        print("Downloading Latest FormationFlight Firmware")
+        download_latest_release_bin_assets("FormationFlight", "FormationFlight")
+        print("Generating install Manifests")
+        generate_manifests()
+        time.sleep(60)
 
 
 if __name__ == "__main__":
